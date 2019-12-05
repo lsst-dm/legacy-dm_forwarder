b6631812ee24d12bd5934033bfce2c3dc0a45fbc
Htut Khine Win <hwin16@illinois.edu>
2019-12-05 12:13:49 -0600

    Added OBSID to processingStatus message

--- src/forwarder/miniforwarder.cpp
+++ src/forwarder/miniforwarder.cpp
@@ -33,14 +33,14 @@
 
 namespace fs = boost::filesystem;
 
-miniforwarder::miniforwarder(const std::string& config, 
+miniforwarder::miniforwarder(const std::string& config,
                              const std::string& log) : IIPBase(config, log)
                                  , _hdr()
                                  , _readoutpattern(_config_root) {
     std::string work_dir, ip_host, redis_host, xfer_option;
     int redis_port, redis_db;
     std::vector<std::string> segment_order;
-    try { 
+    try {
         work_dir = _config_root["WORK_DIR"].as<std::string>();
         ip_host = _config_root["BASE_BROKER_ADDR"].as<std::string>();
         redis_host = _config_root["REDIS_HOST"].as<std::string>();
@@ -60,56 +60,56 @@ miniforwarder::miniforwarder(const std::string& config,
         _seconds_to_update = _config_root["SECONDS_TO_UPDATE"].as<int>();
         _seconds_to_expire = _config_root["SECONDS_TO_EXPIRE"].as<int>();
     }
-    catch (YAML::TypedBadConversion<std::string>& e) { 
+    catch (YAML::TypedBadConversion<std::string>& e) {
         LOG_CRT << "YAML bad conversion for std::string";
-        exit(EXIT_FAILURE); 
+        exit(EXIT_FAILURE);
     }
-    catch (YAML::TypedBadConversion<int>& e) { 
+    catch (YAML::TypedBadConversion<int>& e) {
         LOG_CRT << "YAML bad conversion for int";
-        exit(EXIT_FAILURE); 
+        exit(EXIT_FAILURE);
     }
-    catch (YAML::TypedBadConversion<std::vector<std::string>>& e) { 
+    catch (YAML::TypedBadConversion<std::vector<std::string>>& e) {
         LOG_CRT << "YAML bad conversion for vector<string>";
-        exit(EXIT_FAILURE); 
+        exit(EXIT_FAILURE);
     }
 
     const std::string user = _credentials->get_user("service_user");
     const std::string passwd = _credentials->get_user("service_passwd");
     _amqp_url = "amqp://" + user + ":" + passwd + "@" + ip_host;
 
-    try { 
-        const fs::path header_dir = fs::path(work_dir) / fs::path("header"); 
+    try {
+        const fs::path header_dir = fs::path(work_dir) / fs::path("header");
         const fs::path fits_dir = fs::path(work_dir) / fs::path("fits");
         _header_path = create_dir(header_dir);
         _fits_path = create_dir(fits_dir);
-    } 
-    catch (L1::CannotCreateDir& e) { 
-        exit(EXIT_FAILURE); 
+    }
+    catch (L1::CannotCreateDir& e) {
+        exit(EXIT_FAILURE);
     }
 
     // TODO
     const std::string redis_pwd = "meh";
 
     _actions = {
-        { "AT_FWDR_HEALTH_CHECK", std::bind(&miniforwarder::health_check, 
+        { "AT_FWDR_HEALTH_CHECK", std::bind(&miniforwarder::health_check,
                 this, std::placeholders::_1) },
-        { "AT_FWDR_XFER_PARAMS", std::bind(&miniforwarder::xfer_params, 
+        { "AT_FWDR_XFER_PARAMS", std::bind(&miniforwarder::xfer_params,
                 this, std::placeholders::_1) },
-        { "AT_FWDR_HEADER_READY", std::bind(&miniforwarder::header_ready, 
+        { "AT_FWDR_HEADER_READY", std::bind(&miniforwarder::header_ready,
                 this, std::placeholders::_1) },
-        { "AT_FWDR_END_READOUT", std::bind(&miniforwarder::end_readout, 
+        { "AT_FWDR_END_READOUT", std::bind(&miniforwarder::end_readout,
                 this, std::placeholders::_1) },
-        { "FILE_TRANSFER_COMPLETED_ACK", std::bind(&miniforwarder::process_ack, 
+        { "FILE_TRANSFER_COMPLETED_ACK", std::bind(&miniforwarder::process_ack,
                 this, std::placeholders::_1) },
-        { "ASSOCIATED", std::bind(&miniforwarder::associated, 
+        { "ASSOCIATED", std::bind(&miniforwarder::associated,
                 this, std::placeholders::_1) }
     };
 
     try {
         _pub = std::unique_ptr<SimplePublisher>(new SimplePublisher(_amqp_url));
-    } 
-    catch (L1::PublisherError& e) { 
-        exit(EXIT_FAILURE); 
+    }
+    catch (L1::PublisherError& e) {
+        exit(EXIT_FAILURE);
     }
 
     _db = std::unique_ptr<Scoreboard>(
@@ -121,8 +121,8 @@ miniforwarder::miniforwarder(const std::string& config,
 
     _forwarder_list = "forwarder_list";
     _association_key = "f99_association";
-    
-    auto bound_register_fwd = std::bind(&miniforwarder::register_fwd, this); 
+   
+    auto bound_register_fwd = std::bind(&miniforwarder::register_fwd, this);
     _hb_params.seconds_to_expire = _seconds_to_expire;
     _hb_params.seconds_to_update = _seconds_to_update;
     _hb_params.key = _association_key;
@@ -137,10 +137,10 @@ miniforwarder::miniforwarder(const std::string& config,
     _watcher = std::unique_ptr<Watcher>(new Watcher());
 }
 
-miniforwarder::~miniforwarder() { 
+miniforwarder::~miniforwarder() {
 }
 
-void miniforwarder::on_message(const std::string& message) { 
+void miniforwarder::on_message(const std::string& message) {
     try {
         LOG_DBG << "Received message " << message;
         const YAML::Node n = YAML::Load(message);
@@ -152,26 +152,26 @@ void miniforwarder::on_message(const std::string& message) {
     }
 }
 
-void miniforwarder::run() { 
-    try { 
+void miniforwarder::run() {
+    try {
         Consumer consumer(_amqp_url, _consume_q);
-        auto on_msg = bind(&miniforwarder::on_message, this, 
+        auto on_msg = bind(&miniforwarder::on_message, this,
                 std::placeholders::_1);
         consumer.run(on_msg);
-    } 
+    }
     catch (L1::ConsumerError& e) { exit(-1); }
-    catch (std::exception& e) { 
+    catch (std::exception& e) {
         LOG_CRT << e.what();
         exit(-1);
     }
 }
 
-void miniforwarder::health_check(const YAML::Node& n) { 
+void miniforwarder::health_check(const YAML::Node& n) {
     publish_ack(n);
 }
 
 void miniforwarder::xfer_params(const YAML::Node& n) {
-    try { 
+    try {
         publish_ack(n);
 
         const std::string image_id = n["IMAGE_ID"].as<std::string>();
@@ -186,52 +186,67 @@ void miniforwarder::xfer_params(const YAML::Node& n) {
 
         _db->add_xfer(image_id, xfer);
     }
-    catch (std::exception& e) { 
+    catch (std::exception& e) {
         LOG_CRT << e.what();
     }
 }
 
 void miniforwarder::header_ready(const YAML::Node& n) {
-    try { 
-        publish_ack(n);
+    publish_ack(n);
 
-        const std::string filename = n["FILENAME"].as<std::string>();
-        const std::string image_id = n["IMAGE_ID"].as<std::string>();
-        const std::string reply_q = n["REPLY_QUEUE"].as<std::string>();
-        const std::string ack_id = n["ACK_ID"].as<std::string>();
+    std::string filename, image_id, reply_q, ack_id;
+    try {
+        filename = n["FILENAME"].as<std::string>();
+        image_id = n["IMAGE_ID"].as<std::string>();
+        reply_q = n["REPLY_QUEUE"].as<std::string>();
+        ack_id = n["ACK_ID"].as<std::string>();
+    }
+    catch (YAML::TypedBadConversion<std::string>& e) {
+        LOG_CRT << "Cannot read field from YAML Node";
+        return;
+    }
 
+    try {
         fs::path header = _header_path / fs::path(image_id);
         _hdr.fetch(filename, header);
         _db->add(image_id, "header_ready");
         assemble(image_id);
     }
-    catch (L1::CannotFetchHeader& e) { 
+    catch (L1::CannotFetchHeader& e) {
         int error_code = 5610;
-        publish_processing_status(error_code, e.what()); 
+        publish_processing_status(error_code, image_id, e.what());
     }
-    catch (std::exception& e) { 
+    catch (std::exception& e) {
         LOG_CRT << e.what();
         int error_code = 5610;
-        publish_processing_status(error_code, e.what()); 
+        publish_processing_status(error_code, image_id, e.what());
     }
 }
 
 void miniforwarder::end_readout(const YAML::Node& n) {
-    try { 
-        publish_ack(n);
+    publish_ack(n);
 
-        const std::string image_id = n["IMAGE_ID"].as<std::string>();
+    std::string image_id;
+    try {
+        image_id = n["IMAGE_ID"].as<std::string>();
+    }
+    catch (YAML::TypedBadConversion<std::string>& e) {
+        LOG_CRT << "Cannot read IMAGE_ID from YAML Node";
+        return;
+    }
+
+    try {
         const xfer_info xfer = _db->get_xfer(image_id);
         const std::string raft = xfer.raft;
         const std::vector<std::string> ccds = xfer.ccds;
         const std::string sensor = "WFS";
 
-        for (auto& ccd : ccds) { 
-            const std::string filename = image_id + "--R" + raft + 
+        for (auto& ccd : ccds) {
+            const std::string filename = image_id + "--R" + raft +
                 "S" + ccd + ".fits";
             const fs::path filepath = _fits_path / fs::path(filename);
-            if (!check_valid_board(raft, ccd)) { 
-                std::string err = "Raft/ccd " + raft + "/" + ccd + 
+            if (!check_valid_board(raft, ccd)) {
+                std::string err = "Raft/ccd " + raft + "/" + ccd +
                     " does not exist in partition " + _partition;
                 LOG_CRT << err;
                 throw L1::CannotFetchPixel(err);
@@ -243,33 +258,33 @@ void miniforwarder::end_readout(const YAML::Node& n) {
         _db->add(image_id, "end_readout");
         assemble(image_id);
     }
-    catch (L1::KeyNotFound& e) { 
+    catch (L1::KeyNotFound& e) {
         int error_code = 5611;
-        publish_processing_status(error_code, e.what()); 
+        publish_processing_status(error_code, image_id, e.what());
     }
     catch (L1::CannotFetchPixel& e) {
         int error_code = 5611;
-        publish_processing_status(error_code, e.what()); 
+        publish_processing_status(error_code, image_id, e.what());
     }
-    catch (std::exception& e) { 
+    catch (std::exception& e) {
         LOG_CRT << e.what();
         int error_code = 5611;
-        publish_processing_status(error_code, e.what()); 
+        publish_processing_status(error_code, image_id, e.what());
     }
 }
 
-void miniforwarder::process_ack(const YAML::Node& n) { 
-    try { 
+void miniforwarder::process_ack(const YAML::Node& n) {
+    try {
         const std::string msg_type = n["MSG_TYPE"].as<std::string>();
         LOG_INF << "Got ack for message type " << msg_type;
     }
-    catch (std::exception& e) { 
+    catch (std::exception& e) {
         LOG_CRT << e.what();
     }
 }
 
 void miniforwarder::associated(const YAML::Node& n) {
-    try { 
+    try {
         const std::string key = n["ASSOCIATION_KEY"].as<std::string>();
         const std::string reply_q = n["REPLY_QUEUE"].as<std::string>();
         const std::string ack_id = n["ACK_ID"].as<std::string>();
@@ -282,17 +297,17 @@ void miniforwarder::associated(const YAML::Node& n) {
         params.key = key;
         _watcher->start(params);
     }
-    catch(std::exception& e) { 
+    catch(std::exception& e) {
         LOG_CRT << e.what();
     }
 }
 
-void miniforwarder::publish_ack(const YAML::Node& n) { 
-    try { 
+void miniforwarder::publish_ack(const YAML::Node& n) {
+    try {
         const std::string msg_type = n["MSG_TYPE"].as<std::string>();
         const std::string ack_id = n["ACK_ID"].as<std::string>();
         const std::string reply_q = n["REPLY_QUEUE"].as<std::string>();
-        const std::string msg = _builder.build_ack(msg_type, _name, 
+        const std::string msg = _builder.build_ack(msg_type, _name,
                 ack_id, "True");
         _pub->publish_message(reply_q, msg);
     }
@@ -305,9 +320,9 @@ void miniforwarder::publish_ack(const YAML::Node& n) {
 void miniforwarder::publish_xfer_complete(const std::string& to,
                                           const std::string& session_id,
                                           const std::string& job_num) {
-    try { 
+    try {
         const std::string filename = to.substr(to.find(":") + 1);
-        const std::string msg = _builder.build_xfer_complete(filename, 
+        const std::string msg = _builder.build_xfer_complete(filename,
                 session_id, job_num, _consume_q);
         _pub->publish_message(_archive_q, msg);
     }
@@ -318,17 +333,18 @@ void miniforwarder::publish_xfer_complete(const std::string& to,
 }
 
 void miniforwarder::publish_processing_status(const int& error_code,
+                                              const std::string& obsid,
                                               const std::string& desc) {
-    const std::string msg = _builder.build_processing_status(error_code, desc);
-    try { 
+    const std::string msg = _builder.build_processing_status(error_code, obsid, desc);
+    try {
         _pub->publish_message(_telemetry_q, msg);
     }
     catch (L1::PublisherError& e) {}
 }
 
-void miniforwarder::assemble(const std::string& image_id) { 
-    try { 
-        if (_db->is_ready(image_id)) { 
+void miniforwarder::assemble(const std::string& image_id) {
+    try {
+        if (_db->is_ready(image_id)) {
             const xfer_info xfer = _db->get_xfer(image_id);
             const std::string raft = xfer.raft;
             const std::vector<std::string> ccds = xfer.ccds;
@@ -336,8 +352,8 @@ void miniforwarder::assemble(const std::string& image_id) {
             const std::string job_num = xfer.job_num;
             const fs::path header = _header_path / fs::path(image_id);
 
-            for (auto& ccd : ccds) { 
-                const std::string filename = image_id + "--R" + raft + 
+            for (auto& ccd : ccds) {
+                const std::string filename = image_id + "--R" + raft +
                     "S" + ccd + ".fits";
                 const fs::path pix = _fits_path / fs::path(filename);
                 const fs::path to = fs::path(xfer.target) / fs::path(filename);
@@ -345,12 +361,12 @@ void miniforwarder::assemble(const std::string& image_id) {
                 _fmt->write_header(pix, header);
                 _sender->send(pix, to);
                 publish_xfer_complete(to.string(), session_id, job_num);
-                
+               
                 LOG_INF << "********* READOUT COMPLETE for " << image_id;
 
                 const std::string msg = filename +
                     " is successfuly transferred to " + to.string();
-                publish_processing_status(1, msg);
+                publish_processing_status(1, image_id, msg);
 
                 _db->remove(image_id);
                 std::remove(pix.c_str());
@@ -361,40 +377,40 @@ void miniforwarder::assemble(const std::string& image_id) {
     }
     catch (L1::KeyNotFound& e) {
         int error_code = 5612;
-        publish_processing_status(error_code, e.what()); 
+        publish_processing_status(error_code, image_id, e.what());
     }
     catch (L1::InvalidReadoutPattern& e) {
         int error_code = 5612;
-        publish_processing_status(error_code, e.what()); 
+        publish_processing_status(error_code, image_id, e.what());
     }
     catch (L1::CannotFormatFitsfile& e) {
         int error_code = 5612;
-        publish_processing_status(error_code, e.what()); 
+        publish_processing_status(error_code, image_id, e.what());
     }
     catch (L1::CannotCopyFile& e) {
         int error_code = 5612;
-        publish_processing_status(error_code, e.what()); 
-    } 
-    catch (std::exception& e) { 
-        std::string err = "Cannot assemble fitsfile because " + 
+        publish_processing_status(error_code, image_id, e.what());
+    }
+    catch (std::exception& e) {
+        std::string err = "Cannot assemble fitsfile because " +
             std::string(e.what());
-        LOG_CRT << err; 
+        LOG_CRT << err;
         int error_code = 5612;
-        publish_processing_status(error_code, e.what()); 
+        publish_processing_status(error_code, image_id, e.what());
     }
 }
 
-fs::path miniforwarder::create_dir(const fs::path& file_path) { 
+fs::path miniforwarder::create_dir(const fs::path& file_path) {
     boost::system::error_code err;
 
     // The status is not that useful. status == false doesn't mean directory
-    // creation failed but it means boost::filesystem didn't create that 
-    // directory. In other words, if the directory already exists, status will 
+    // creation failed but it means boost::filesystem didn't create that
+    // directory. In other words, if the directory already exists, status will
     // be false, regardless of who created it.
     bool status = create_directories(file_path, err);
 
     if (err.value()) {
-        std::string err_msg = "Cannot create directory for path " 
+        std::string err_msg = "Cannot create directory for path "
                 + file_path.string() + " because " + err.message();
         LOG_CRT << err_msg;
         throw L1::CannotCreateDir(err_msg);
@@ -402,7 +418,7 @@ fs::path miniforwarder::create_dir(const fs::path& file_path) {
     return file_path;
 }
 
-void miniforwarder::register_fwd() { 
+void miniforwarder::register_fwd() {
     char hostname[HOST_NAME_MAX];
     gethostname(hostname, HOST_NAME_MAX);
 
@@ -411,7 +427,7 @@ void miniforwarder::register_fwd() {
     addr.ai_family = AF_INET;
     // TODO: RAII
     int response = getaddrinfo(hostname, NULL, NULL, &infoptr);
-    if (response) { 
+    if (response) {
         LOG_CRT << "Cannot get hostname";
         throw L1::L1Exception("Cannot get hostname");
     }
@@ -419,8 +435,8 @@ void miniforwarder::register_fwd() {
     struct addrinfo *p;
     char host[256];
     std::string ip_addr;
-    for (p = infoptr; p != NULL; p = p->ai_next) { 
-        getnameinfo(p->ai_addr, p->ai_addrlen, host, sizeof(host), NULL, 0, 
+    for (p = infoptr; p != NULL; p = p->ai_next) {
+        getnameinfo(p->ai_addr, p->ai_addrlen, host, sizeof(host), NULL, 0,
                 NI_NUMERICHOST);
         ip_addr = host;
         break;
@@ -432,12 +448,12 @@ void miniforwarder::register_fwd() {
     LOG_INF << "Set forwarder in redis list";
 }
 
-bool miniforwarder::check_valid_board(const std::string& raft, 
+bool miniforwarder::check_valid_board(const std::string& raft,
                                       const std::string& ccd) {
-    const std::string bay_board = raft + "/" + ccd[0]; 
-    auto found = std::find(_daq_locations.begin(), _daq_locations.end(), 
+    const std::string bay_board = raft + "/" + ccd[0];
+    auto found = std::find(_daq_locations.begin(), _daq_locations.end(),
             bay_board);
-    if (found == _daq_locations.end()) { 
+    if (found == _daq_locations.end()) {
         return false;
     }
     return true;
