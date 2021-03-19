d3d018bdb42913f9c18443759387c42978826abe
Stephen R Pietrowicz <srp@illinois.edu>
2021-03-19 15:19:55 -0500

    Add missing IIP_CONFIG_DIR environment var Remove container version from run.sh

--- run.sh
+++ run.sh
@@ -4,13 +4,12 @@ export IIP_UID=$(id -u iip)
 export IIP_GID=$(id -g iip)
 export KRB5CCNAME_FILE=/tmp/krb5cc_$(id -u iip)
 
-error_msg="$0: missing argument: -d [latiss|comcam] -s [summit|ncsa] -c container_version"
+error_msg="$0: missing argument: -d [latiss|comcam] -s [summit|ncsa]"
 
 export IIP_CONFIG_DIR=
 export IIP_DEVICE=
 export IIP_SITE=
-export IIP_CONTAINER_VERSION=
-while getopts d:s:c: option
+while getopts d:s: option
 do
 case "${option}"
 in
@@ -28,11 +27,10 @@ s)
         echo "-s argument must be summit or ncsa"
         exit 1
     fi;;
-c) IIP_CONTAINER_VERSION=${OPTARG};;
 esac
 done
 
-if [[ -z $IIP_DEVICE ]] || [[ -z $IIP_SITE ]] || [[ -z $IIP_CONTAINER_VERSION ]]; then
+if [[ -z $IIP_DEVICE ]] || [[ -z $IIP_SITE ]]; then
     echo $error_msg
     exit 1
 fi
@@ -42,7 +40,6 @@ IIP_CONFIG_DIR=/opt/lsst/dm_forwarder/config/$IIP_SITE/$IIP_DEVICE
 echo "IIP_CONFIG_DIR="$IIP_CONFIG_DIR
 echo "IIP_DEVICE = "$IIP_DEVICE
 echo "IIP_SITE = "$IIP_SITE
-echo "IIP_CONTAINER_VERSION = "$IIP_CONTAINER_VERSION
 echo "KRB5CCNAME_FILE= "$KRB5CCNAME_FILE
 
 docker-compose -f docker-compose.yml up -d dm_forwarder
