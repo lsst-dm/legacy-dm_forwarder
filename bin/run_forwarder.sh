521f91d31a876db5fda2e51a39fb9c028ba7acaf
Stephen R Pietrowicz <srp@illinois.edu>
2021-04-13 16:41:40 -0500

    Rename run.sh to bin/run_forwarder.sh

--- bin/run_forwarder.sh
+++ bin/run_forwarder.sh
@@ -1,26 +1,55 @@
 #!/bin/bash
-export FORWARDER_USER=iip
 
-if [ -z $1 ];
-then
-    echo "$0: missing argument: container_version"
+export IIP_UID=$(id -u iip)
+export IIP_GID=$(id -g iip)
+export KRB5CCNAME_FILE=/tmp/krb5cc_$(id -u iip)
+
+error_msg="$0: missing argument: -d [latiss|comcam] -s [summit|nts]"
+
+export IIP_CONFIG_DIR=
+export IIP_DEVICE=
+export IIP_SITE=
+export IIP_CONTAINER=
+
+while getopts d:s: option
+do
+case "${option}"
+in
+d) 
+    if [ "$OPTARG" = "latiss" ] || [ "$OPTARG" = "comcam" ]; then
+        IIP_DEVICE=${OPTARG}
+    else
+        echo "-d argument must be latiss or comcam"
+        exit 1
+    fi;;
+s) 
+    if [ "$OPTARG" = "summit" ] || [ "$OPTARG" = "nts" ]; then
+        IIP_SITE=${OPTARG}
+    else
+        echo "-s argument must be summit or nts"
+        exit 1
+    fi;;
+esac
+done
+
+if [[ -z $IIP_DEVICE ]] || [[ -z $IIP_SITE ]]; then
+    echo $error_msg
     exit 1
 fi
 
-container_version=$1
+if [ "$IIP_SITE" = "summit" ]; then
+    IIP_CONTAINER=fwd_summit
+else 
+    # IIP_CONTAINER=fwd_${IIP_SITE}_${IIP_DEVICE}
+    IIP_CONTAINER=fwd_nts
+fi;
 
-docker run \
-    -d \
-    -u `id -u iip`:`id -g iip` \
-    --name dm_forwarder \
-    --network host \
-    --pid host \
-    -v /home/iip/.ssh:/home/iip/.ssh \
-    -v /home/iip/.lsst:/home/iip/.lsst \
-    -v /tmp:/tmp \
-    -v /var/log/iip:/var/log/iip \
-    -v /etc/passwd:/etc/passwd \
-    -v /etc/group:/etc/group \
-    -v /opt/lsst/dm_forwarder/config:/opt/lsst/dm_forwarder/config \
-    -v /home/lsst-daq/images:/home/lsst-daq/images \
-    ts-dockerhub.lsst.org/lsstts/dm_forwarder:$container_version
+IIP_CONFIG_DIR=/opt/lsst/dm_forwarder/config/$IIP_SITE/$IIP_DEVICE
+
+echo "IIP_CONFIG_DIR="$IIP_CONFIG_DIR
+echo "IIP_DEVICE = "$IIP_DEVICE
+echo "IIP_SITE = "$IIP_SITE
+echo "KRB5CCNAME_FILE= "$KRB5CCNAME_FILE
+echo "IIP_CONTAINER= "$IIP_CONTAINER
+
+docker-compose -f docker-compose.yml up -d $IIP_CONTAINER
