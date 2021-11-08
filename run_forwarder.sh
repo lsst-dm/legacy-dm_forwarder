68064654c81edf28df0453c5d828b5439667d99f
Stephen R Pietrowicz <srp@illinois.edu>
2021-11-08 20:59:31 +0000

    Changes to allow local config directories to mount into containers

--- run_forwarder.sh
+++ run_forwarder.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+SCRIPTPATH=$(readlink -f "$0")
+BASEDIR=$(dirname "$SCRIPTPATH")
 export IIP_UID=$(id -u iip)
 export IIP_GID=$(id -g iip)
 export KRB5CCNAME_FILE=/tmp/krb5cc_$(id -u iip)
 
-error_msg="$0: missing argument: -d [latiss|comcam] -s [summit|nts]"
+error_msg="$0: missing argument: -d [latiss|comcam] -s [summit|nts|tucson]"
 
+export LOCAL_CONFIG_DIR=
 export IIP_CONFIG_DIR=
 export IIP_DEVICE=
 export IIP_SITE=
@@ -23,10 +26,10 @@ d)
         exit 1
     fi;;
 s) 
-    if [ "$OPTARG" = "summit" ] || [ "$OPTARG" = "nts" ]; then
+    if [ "$OPTARG" = "summit" ] || [ "$OPTARG" = "nts" ] || [ "$OPTARG" = "tucson" ]; then
         IIP_SITE=${OPTARG}
     else
-        echo "-s argument must be summit or nts"
+        echo "-s argument must be summit, nts or tucson"
         exit 1
     fi;;
 esac
@@ -39,11 +42,14 @@ fi
 
 if [ "$IIP_SITE" = "summit" ]; then
     IIP_CONTAINER=fwd_summit
-else 
+elif [ "$IIP_SITE" = "tucson" ]; then
+    IIP_CONTAINER=fwd_${IIP_SITE}_${IIP_DEVICE}
+else
     # IIP_CONTAINER=fwd_${IIP_SITE}_${IIP_DEVICE}
     IIP_CONTAINER=fwd_nts
 fi;
 
+LOCAL_CONFIG_DIR=$BASEDIR/dm_forwarder/etc/config/$IIP_SITE/$IIP_DEVICE
 IIP_CONFIG_DIR=/opt/lsst/dm_forwarder/config/$IIP_SITE/$IIP_DEVICE
 
 echo "IIP_CONFIG_DIR = "$IIP_CONFIG_DIR
@@ -52,4 +58,5 @@ echo "IIP_SITE = "$IIP_SITE
 echo "KRB5CCNAME_FILE = "$KRB5CCNAME_FILE
 echo "IIP_CONTAINER = "$IIP_CONTAINER
 
+echo $LOCAL_CONFIG_DIR
 docker-compose -f docker-compose.yml up -d $IIP_CONTAINER
